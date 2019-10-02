f29572c618bfafac368c0fce9c9b8910b4087854
Grisel Alpanes <griselmelina@gmail.com>
2019-10-02 14:06:43 +0100

    gulp part 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 "use strict";
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var browserSync = require("browser-sync");
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let browserSync = require("browser-sync");
+
+let del = require("del"),
+  imagemin = require("gulp-imagemin"),
+  uglify = require("gulp-uglify"),
+  usemin = require("gulp-usemin"),
+  rev = require("gulp-rev"),
+  cleanCss = require("gulp-clean-css"),
+  flatmap = require("gulp-flatmap"),
+  htmlmin = require("gulp-htmlmin");
 
 gulp.task("sass", function() {
   return gulp
-    .src(["./css/*.scss"])
+    .src("./css/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./css"));
 });
@@ -16,7 +25,8 @@ gulp.task("sass:watch", function() {
 });
 
 gulp.task("browser-sync", function() {
-  var files = ["./*.html", "./css/*.css", "./js/*.js", "./img/*.{png,jpg,gif}"];
+  var files = ["./*.html", "./css/*.css", "./img/*.{png,jpg,gif}", "./js/*.js"];
+
   browserSync.init(files, {
     server: {
       baseDir: "./"
@@ -24,4 +34,52 @@ gulp.task("browser-sync", function() {
   });
 });
 
+//clean
+gulp.task("clean", function() {
+  return del(["dist"]);
+});
+
+gulp.task("copyfonts", function() {
+  return gulp.src("./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*").pipe(gulp.dest("./dist/fonts"));
+});
+
+//Images
+gulp.task("imagemin", function() {
+  return gulp
+    .src("img/*.{png,jpg,gif}")
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("usemin", function() {
+  return gulp
+    .src("./*.html")
+    .pipe(
+      flatmap(function(stream, file) {
+        return stream.pipe(
+          usemin({
+            css: [rev()],
+            html: [
+              function() {
+                return htmlmin({ collapseWhitespace: true });
+              }
+            ],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), "concat"]
+          })
+        );
+      })
+    )
+    .pipe(gulp.dest("dist/"));
+});
+
+// Default task
 gulp.task("default", gulp.series("browser-sync", "sass:watch"));
+
+gulp.task(
+  "build",
+  gulp.series("clean", "copyfonts", "imagemin", "usemin", function(done) {
+    done();
+  })
+);
